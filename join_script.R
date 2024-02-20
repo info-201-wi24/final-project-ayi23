86eeac33b8089b44c0fe50c3faffcf92254029e1
Renee Chien <rkc9@uw.edu>
2024-02-20 12:30:14 -0800

    still having a join error

--- join_script.R
+++ join_script.R
@@ -5,7 +5,13 @@ csv1 <- read.csv("2015 Suicide Rates By State.csv")
 csv2 <- read.csv("StateExpenditures.csv")
 
 # Join CSV files based on the "STATE" column
-joined_data <- inner_join(csv1, csv2, by = c("STATE" = "state"))
+joined_data <- left_join(csv1, csv2, by = "STATE")
 
 # Write joined data to a new CSV file
 write.csv(joined_data, "State Expenditures and Suicide Rates.csv", row.names = FALSE)
+
+#one new categorical variable
+
+#one new numerical variable (continuous)
+
+#one data summarization table
