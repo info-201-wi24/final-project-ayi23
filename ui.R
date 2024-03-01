a0915b4fe28ba5348106ed693c5d315299401d85
pisshub <159188270+pisshub@users.noreply.github.com>
2024-03-01 13:30:37 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,24 +2,24 @@
 
 ## OVERVIEW TAB INFO
 
-overview_tab <- tabPanel("Overview Tab Title",
-   h1("Some title"),
-   p("some explanation")
+overview_tab <- tabPanel("Overview",
+   h1("Mental Health Expenditures & Suicide Rates"),
+   p("WIP")
 )
 
 ## VIZ 1 TAB INFO
 
 viz_1_sidebar <- sidebarPanel(
-  h2("Options for graph"),
+  h2("WIP heatmap"),
   #TODO: Put inputs for modifying graph here
 )
 
 viz_1_main_panel <- mainPanel(
-  h2("Vizualization 1 Title"),
+  h2("Heatmap of Suicide Rates"),
   # plotlyOutput(outputId = "your_viz_1_output_id")
 )
 
-viz_1_tab <- tabPanel("Viz 1 tab title",
+viz_1_tab <- tabPanel("Suicide Rates per State",
   sidebarLayout(
     viz_1_sidebar,
     viz_1_main_panel
@@ -29,16 +29,16 @@ viz_1_tab <- tabPanel("Viz 1 tab title",
 ## VIZ 2 TAB INFO
 
 viz_2_sidebar <- sidebarPanel(
-  h2("Options for graph"),
+  h2("WIP heatmap"),
   #TODO: Put inputs for modifying graph here
 )
 
 viz_2_main_panel <- mainPanel(
-  h2("Vizualization 2 Title"),
+  h2("Heatmap of State Mental Health Expenditures"),
   # plotlyOutput(outputId = "your_viz_1_output_id")
 )
 
-viz_2_tab <- tabPanel("Viz 2 tab title",
+viz_2_tab <- tabPanel("MH Expenditure per State",
   sidebarLayout(
     viz_2_sidebar,
     viz_2_main_panel
@@ -53,11 +53,11 @@ viz_3_sidebar <- sidebarPanel(
 )
 
 viz_3_main_panel <- mainPanel(
-  h2("Vizualization 3 Title"),
+  h2("Scatterplot of MH Expenditures and Suicide Rates"),
   # plotlyOutput(outputId = "your_viz_1_output_id")
 )
 
-viz_3_tab <- tabPanel("Viz 3 tab title",
+viz_3_tab <- tabPanel("Correlation",
   sidebarLayout(
     viz_3_sidebar,
     viz_3_main_panel
@@ -66,14 +66,14 @@ viz_3_tab <- tabPanel("Viz 3 tab title",
 
 ## CONCLUSIONS TAB INFO
 
-conclusion_tab <- tabPanel("Conclusion Tab Title",
- h1("Some title"),
- p("some conclusions")
+conclusion_tab <- tabPanel("Closing",
+ h1("Conclusion"),
+ p("WIP concluding statements")
 )
 
 
 
-ui <- navbarPage("Example Project Title",
+ui <- navbarPage("US Mental Health Budget",
   overview_tab,
   viz_1_tab,
   viz_2_tab,
